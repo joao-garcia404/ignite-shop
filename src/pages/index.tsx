8445b5f55dc6eb284d2a4c4e733bb8b94d9d1f53
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-10-15 11:07:01 -0300

    Chore: Removing mock images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,6 @@ import { useKeenSlider } from "keen-slider/react";
 
 import { HomeContainer, Product } from "../styles/pages/home";
 
-import camiseta1 from "../assets/camiseta1.png";
-import camiseta2 from "../assets/camiseta2.png";
-import camiseta3 from "../assets/camiseta3.png";
-
 import "keen-slider/keen-slider.min.css";
 import Stripe from "stripe";
 
