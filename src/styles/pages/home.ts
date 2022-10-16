25400bd83368de90548b6f417f8654c20b0623fb
Joao Garcia <jvdasilvaeirasgarcia@gmail.com>
2022-10-16 16:13:07 -0300

    Feat: Success page

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -8,7 +8,7 @@ export const HomeContainer = styled("main", {
   minHeight: 656,
 });
 
-export const Product = styled("a", {
+export const Product = styled("div", {
   background: "linear-gradient(180deg, #1ea484 0%, #7465d4 100%)",
   borderRadius: 8,
   cursor: "pointer",
@@ -23,6 +23,7 @@ export const Product = styled("a", {
 
   img: {
     objectFit: "cover",
+    userSelect: "none",
   },
 
   footer: {
